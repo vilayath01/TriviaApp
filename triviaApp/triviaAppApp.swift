ec6a2d6843d16c658d661b69a5d377601e10899e
Vilayath Mohammed <vilayath.mohammed@arq.group>
2024-01-22 15:34:37 +1100

    Initial Commit

---
+++ triviaApp/triviaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  triviaAppApp.swift
+//  triviaApp
+//
+//  Created by vila on 22/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct triviaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
