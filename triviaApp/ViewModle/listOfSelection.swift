6a4761c059f9269502e2a57b266be062dbd74145
Vilayath Mohammed <vilayath.mohammed@arq.group>
2024-01-27 00:42:49 +1100

    this commit has a categories list for each button selection and also few func to pass the categories to url link did first rebase

--- triviaApp/ViewModle/listOfSelection.swift
+++ triviaApp/ViewModle/listOfSelection.swift
@@ -2,26 +2,29 @@
 import SwiftUI
 
 struct listOfSelection: View {
-    @Binding  var selectedButtonText1: String
-    @Binding var selectedButtonText2: String
-    @Binding var selectedButtonText3: String
-    @Binding var selectedButtonText4: String
+   @Binding var amt: String
+   @Binding var catego: String
+   @Binding var diff: String
+   @Binding var typ: String
     var numberArray: [String] {
         return (1...50).map {String($0)}
     }
+    
+    var categoriesList = CategoriesList()
     
     var body: some View {
         
         VStack {
             
-            RoundedGradientButton(buttonText: self.$selectedButtonText1, categories: numberArray, labelText: "Number of Questions:")
-            RoundedGradientButton(buttonText: self.$selectedButtonText2, categories: ["Vilayath","Junaid"], labelText: "Select Category:")
-            RoundedGradientButton(buttonText: self.$selectedButtonText3, categories: ["Vilayath","Junaid"], labelText: "Select Difficulty:")
-            RoundedGradientButton(buttonText: self.$selectedButtonText4, categories: ["Vilayath","Junaid"], labelText: "Select Type:")
+            
+            RoundedGradientButton(buttonText: self.$amt, categories: numberArray, labelText: "Number of Questions:")
+            RoundedGradientButton(buttonText: self.$catego, categories: Array(categoriesList.nameValueMappingCategory.keys) , labelText: "Select Category:")
+            RoundedGradientButton(buttonText: self.$diff, categories:  Array(categoriesList.nameValueMappingDifficulty.keys) , labelText: "Select Difficulty:")
+            RoundedGradientButton(buttonText: self.$typ, categories: Array(categoriesList.nameValueMappingType.keys) , labelText: "Select Type:")
         }
     }
 }
 
 #Preview {
-    listOfSelection(selectedButtonText1: .constant(""), selectedButtonText2: .constant(""), selectedButtonText3: .constant(""), selectedButtonText4: .constant(""))
+    listOfSelection(amt: .constant(""), catego: .constant(""), diff: .constant(""), typ: .constant(""))
 }
